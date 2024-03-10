cf77517d652f469b7088fc1bb201a73908124c21
jhostynac <jhostyn802@gmail.com>
2024-03-10 09:21:32 -0500

    vv

--- frontend/src/componentes/landing_cursos.tsx
+++ frontend/src/componentes/landing_cursos.tsx
@@ -4,9 +4,10 @@ const Landing_cursos = () => {
         <>
             <div className="landing_cursos">
                 <div className="titulo_jaime"><h1>Cursos</h1></div>
-                <div className="curso_transaccion"></div>
-                <div className="curso_compra"></div>
-                <div className="curso_funcion"></div>
+                <div className="curso_transaccion"><h2>Transacciones</h2><h3>Que bobada ome</h3></div>
+                <div className="curso_compra"><h2>Funcionamiento</h2><h3>:cccccccc</h3></div>
+                <div className="curso_funcion"><h2>Compra/venta</h2>
+                <h3>:cccccc</h3></div>
             </div>
         </>
     )
